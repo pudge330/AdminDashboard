2432ff09fdd2fc1343d31c663793c7f852470982
Brandon <b.garcia0429@outlook.com>
2021-09-06 01:00:14 -0400

    update(index): cleaned up index.php file

--- index.php
+++ index.php
@@ -14,10 +14,8 @@ $headerMenuLinks[2]['links'][1]['active'] = false;
 $data = [
 	// 'contentOnly' => true,
 	'version' => 'Skeleton 1.0',
-	// 'title' => trim(str_repeat('Admin Skeleton 1.0 ', 10)),
 	'title' => 'Admin Skeleton 1.0',
 	'titleUrl' => '#',
-	// 'titleUrlAttributes' => Array(),
 	'memberImage' => 'user.svg',
 	'memberLabel' => 'Skeleton Admin',
 	'lockIcon' => true,
@@ -28,39 +26,8 @@ $data = [
 		'links' => $headerMenuLinks
 	],
 	'content' => [
-		// 'headerMenu' => '&nbsp;',
-		// 'mainMenu' => '&nbsp;',
 		'sideMenu' => '&nbsp;',
-		'appMenu' => '&nbsp;',
-		'page' => '&nbsp;'
-		// 'headerMenu' => function($data) {
-		// 	echo "<ul>";
-		// 	for ($i = 1; $i <= 20; $i++) {
-		// 		echo "<li>Menu Item {$i}</li>";
-		// 	}
-		// 	echo "</ul>";
-		// },
-		// 'mainMenu' => function($data) {
-		// 	echo "<ul>";
-		// 	for ($i = 1; $i <= 20; $i++) {
-		// 		echo "<li>Menu Item {$i}</li>";
-		// 	}
-		// 	echo "</ul>";
-		// },
-		// 'sideMenu' => function($data) {
-		// 	echo "<ul>";
-		// 	for ($i = 1; $i <= 20; $i++) {
-		// 		echo "<li>Menu Item {$i}</li>";
-		// 	}
-		// 	echo "</ul>";
-		// },
-		// 'appMenu' => function($data) {
-		// 	echo "<ul>";
-		// 	for ($i = 1; $i <= 20; $i++) {
-		// 		echo "<li>Menu Item {$i}</li>";
-		// 	}
-		// 	echo "</ul>";
-		// }
+		'appMenu' => '&nbsp;'
 		,'page' => file_get_contents(__DIR__ . '/ipsum.txt')
 	]
 ];
